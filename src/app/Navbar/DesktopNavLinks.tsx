2e4358090082a10e143602f132ec89d8449c1639
Fahad <fahad.45854@iqra.edu.com>
2024-10-10 11:03:57 +0500

    created button

--- src/app/Navbar/DesktopNavLinks.tsx
+++ src/app/Navbar/DesktopNavLinks.tsx
@@ -1,8 +1,14 @@
 import Link from 'next/link';
+import Button from '../components/Button/button';
 function DesktopNavLinks() {
     return (
         // <ul className="absolute right-0 flex flex-row space-x-6">
         <ul className="absolute right-0 flex flex-row space-x-1">
+            <li className="text-xl hover:bg-slate-800 hover:text-white w-28 text-center p-[6.5px] hover:transition-opacity duration-500 hover:cursor-pointer rounded-md">
+                <Button>
+                    <Link href="/">Home</Link>
+                </Button>
+            </li>
             <li className="text-xl hover:bg-slate-800 hover:text-white w-28 text-center p-[6.5px] hover:transition-opacity duration-500 hover:cursor-pointer rounded-md">
                 <Link href="/">Home</Link>
             </li>
