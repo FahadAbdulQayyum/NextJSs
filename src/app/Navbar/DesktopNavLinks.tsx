90ac153ee19d6dce5ce1bfd1e18c03e125e94e35
Fahad <fahad.45843@iqra.edu.pk>
2024-10-11 11:03:57 +0500

    created button removed

--- src/app/Navbar/DesktopNavLinks.tsx
+++ src/app/Navbar/DesktopNavLinks.tsx
@@ -6,7 +6,7 @@ function DesktopNavLinks() {
         <ul className="absolute right-0 flex flex-row space-x-1">
             <li className="text-xl hover:bg-slate-800 hover:text-white w-28 text-center p-[6.5px] hover:transition-opacity duration-500 hover:cursor-pointer rounded-md">
                 <Button>
-                    <Link href="/">Home</Link>
+                    <Link href="/"></Link>
                 </Button>
             </li>
             <li className="text-xl hover:bg-slate-800 hover:text-white w-28 text-center p-[6.5px] hover:transition-opacity duration-500 hover:cursor-pointer rounded-md">
