473b3b388600c2df7acf93c9bfc448827770dc31
Fahad <fahad.45843@iqra.edu.pk>
2024-10-12 23:31:57 +0500

    Now smoothly transition

--- src/app/components/Button/button.tsx
+++ src/app/components/Button/button.tsx
@@ -18,7 +18,8 @@ const CustomButton = ({ children, disabled, subNav }: any) => {
         <div
             // className="p-2 text-yellow-50 hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px] hover:cursor-pointer duration-300 translate-x-1"
             // className={`p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center`}
-            className={`p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${cssStyle.fahad}`}
+            // className={`p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${cssStyle.fahad}`}
+            className={`p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${cssStyle.fahad} ${subNav ? 'space-x-1' : ''}`}
         // className={`p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${subNav ? 'space-x-1' : ''} hover:${direction} duration-1000`}
         // onMouseEnter={handleHover}
         // onMouseLeave={rehandleHover}
@@ -29,7 +30,7 @@ const CustomButton = ({ children, disabled, subNav }: any) => {
                 <span
                     className={cssStyle.rotate}
                 >
-                    <IoIosArrowDown />
+                    <IoIosArrowUp />
                 </span>
             }
         </div >
