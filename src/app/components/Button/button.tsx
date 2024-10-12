74c12ac09bb1cc61fec3dfb1511ecccf59af7512
Fahad <fahad.45843@iqra.edu.pk>
2024-10-12 23:28:49 +0500

    arrow smoothly rotate

--- src/app/components/Button/button.tsx
+++ src/app/components/Button/button.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React, { useState } from 'react'
+import cssStyle from './button.module.css'
 import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io'
 
 const CustomButton = ({ children, disabled, subNav }: any) => {
@@ -17,12 +18,20 @@ const CustomButton = ({ children, disabled, subNav }: any) => {
         <div
             // className="p-2 text-yellow-50 hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px] hover:cursor-pointer duration-300 translate-x-1"
             // className={`p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center`}
-            className={`p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${subNav ? 'space-x-1' : ''} hover:${direction} duration-1000`}
-            onMouseEnter={handleHover}
-            onMouseLeave={rehandleHover}
+            className={`p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${cssStyle.fahad}`}
+        // className={`p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${subNav ? 'space-x-1' : ''} hover:${direction} duration-1000`}
+        // onMouseEnter={handleHover}
+        // onMouseLeave={rehandleHover}
         >
             {children}
-            {subNav && (direction ? <IoIosArrowDown /> : <IoIosArrowUp />)}
+            {/* {subNav && (direction ? <IoIosArrowDown /> : <IoIosArrowUp />)} */}
+            {subNav &&
+                <span
+                    className={cssStyle.rotate}
+                >
+                    <IoIosArrowDown />
+                </span>
+            }
         </div >
     )
 }
