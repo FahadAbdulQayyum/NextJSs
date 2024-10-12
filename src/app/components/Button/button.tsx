f1c7238c9793e24563ae74cfe48437df5023aaba
Fahad <fahad.45843@iqra.edu.pk>
2024-10-13 03:01:50 +0500

    Now subnav appears over hover

--- src/app/components/Button/button.tsx
+++ src/app/components/Button/button.tsx
@@ -1,3 +1,94 @@
+// "use client"
+// import cssStyle from './button.module.css'
+// import { IoIosArrowUp } from 'react-icons/io'
+
+// const CustomButton = ({ children, disabled, subNav, sub }: any) => {
+//     return (
+//         <div
+//             className={`relative p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${cssStyle.fahad} ${subNav ? 'space-x-1' : ''}`}
+//         >
+//             {children}
+//             <span
+//             className={cssStyle.show}
+//             >
+//                 {subNav && (
+//                     <span className={cssStyle.rotate}>
+//                         <IoIosArrowUp />
+//                     </span>
+//                 )}
+//                 {sub && sub.length > 0 && (
+//                     // <div className="absolute left-0 flex group-hover:flex flex-col bg-gray-800 rounded shadow-lg">
+//                     <div
+//                         // className="absolute top-10 flex group-hover:flex flex-col bg-gray-800 rounded shadow-lg"
+//                         // className={`absolute top-10 flex group-hover:flex flex-col bg-gray-800 rounded shadow-lg ${cssStyle.show}`}
+//                         className={`absolute top-10 flex group-hover:flex flex-col bg-gray-800 rounded shadow-lg`}
+//                     >
+//                         {sub.map((v: any, index: number) => (
+//                             <span key={index} className="p-2 text-yellow-50 hover:bg-gray-700">{v.name}</span>
+//                         ))}
+//                     </div>
+//                 )}
+//             </span>
+
+//         </div >
+//     )
+// }
+
+// export default CustomButton
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "use client"
+// import cssStyle from './button.module.css'
+// import { IoIosArrowUp } from 'react-icons/io'
+
+// const CustomButton = ({ children, disabled, subNav, sub }: any) => {
+//     return (
+//         <div
+//             className={`relative p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${cssStyle.fahad} ${subNav ? 'space-x-1' : ''}`}
+//         >
+//             {children}
+//             {subNav && (
+//                 <span className={cssStyle.rotate}>
+//                     <IoIosArrowUp />
+//                 </span>
+//             )}
+//             {sub && (
+//                 <div className="absolute left-0 hidden group-hover:flex flex-col bg-gray-800 rounded shadow-lg">
+//                     {
+//                         sub.map((v: any, index: number) => (
+//                             <span key={index} className="p-2 text-yellow-50 hover:bg-gray-700">{v.name}</span>
+//                         ))
+//                     }
+//                 </div>
+//             )
+//             }
+//         </div >
+//     )
+// }
+
+// export default CustomButton
+
+
+
+
+
+
+
+
 "use client"
 import cssStyle from './button.module.css'
 import { IoIosArrowUp } from 'react-icons/io'
@@ -9,7 +100,7 @@ const CustomButton = ({ children, disabled, subNav, sub }: any) => {
     return (
         <div
             // className={`p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${cssStyle.fahad} ${subNav ? 'space-x-1' : ''} ${sub ? 'hidden hover:block' : ''}`}
-            className={`p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${cssStyle.fahad} ${subNav ? 'space-x-1' : ''} `}
+            className={`p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${cssStyle.fahad} ${subNav ? 'space-x-1' : ''} ${cssStyle.show}`}
         >
             {children}
             {subNav &&
@@ -20,7 +111,7 @@ const CustomButton = ({ children, disabled, subNav, sub }: any) => {
                 </span>
             }
             {sub && <div
-                className="invisible hover:visible fixed top-10 flex flex-col"
+                className={`fixed top-10 flex flex-col ${cssStyle.exacthide}`}
             >{sub.map((v: any) => <span>{v.name}</span>)}</div>}
         </div >
     )
