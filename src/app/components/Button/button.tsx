2d23d7f39905a1461ad9185e55734dec12d72cb7
Fahad <fahad.45843@iqra.edu.pk>
2024-10-13 15:32:29 +0500

    removed extra comments

--- src/app/components/Button/button.tsx
+++ src/app/components/Button/button.tsx
@@ -1,105 +1,12 @@
-// "use client"
-// import cssStyle from './button.module.css'
-// import { IoIosArrowUp } from 'react-icons/io'
-
-// const CustomButton = ({ children, disabled, subNav, sub }: any) => {
-//     return (
-//         <div
-//             className={`relative p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${cssStyle.fahad} ${subNav ? 'space-x-1' : ''}`}
-//         >
-//             {children}
-//             <span
-//             className={cssStyle.show}
-//             >
-//                 {subNav && (
-//                     <span className={cssStyle.rotate}>
-//                         <IoIosArrowUp />
-//                     </span>
-//                 )}
-//                 {sub && sub.length > 0 && (
-//                     // <div className="absolute left-0 flex group-hover:flex flex-col bg-gray-800 rounded shadow-lg">
-//                     <div
-//                         // className="absolute top-10 flex group-hover:flex flex-col bg-gray-800 rounded shadow-lg"
-//                         // className={`absolute top-10 flex group-hover:flex flex-col bg-gray-800 rounded shadow-lg ${cssStyle.show}`}
-//                         className={`absolute top-10 flex group-hover:flex flex-col bg-gray-800 rounded shadow-lg`}
-//                     >
-//                         {sub.map((v: any, index: number) => (
-//                             <span key={index} className="p-2 text-yellow-50 hover:bg-gray-700">{v.name}</span>
-//                         ))}
-//                     </div>
-//                 )}
-//             </span>
-
-//         </div >
-//     )
-// }
-
-// export default CustomButton
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// "use client"
-// import cssStyle from './button.module.css'
-// import { IoIosArrowUp } from 'react-icons/io'
-
-// const CustomButton = ({ children, disabled, subNav, sub }: any) => {
-//     return (
-//         <div
-//             className={`relative p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${cssStyle.fahad} ${subNav ? 'space-x-1' : ''}`}
-//         >
-//             {children}
-//             {subNav && (
-//                 <span className={cssStyle.rotate}>
-//                     <IoIosArrowUp />
-//                 </span>
-//             )}
-//             {sub && (
-//                 <div className="absolute left-0 hidden group-hover:flex flex-col bg-gray-800 rounded shadow-lg">
-//                     {
-//                         sub.map((v: any, index: number) => (
-//                             <span key={index} className="p-2 text-yellow-50 hover:bg-gray-700">{v.name}</span>
-//                         ))
-//                     }
-//                 </div>
-//             )
-//             }
-//         </div >
-//     )
-// }
-
-// export default CustomButton
-
-
-
-
-
-
-
-
 "use client"
+
 import cssStyle from './button.module.css'
 import { IoIosArrowUp } from 'react-icons/io'
 
 const CustomButton = ({ children, disabled, subNav, sub }: any) => {
 
-    console.log('children...', children)
-
     return (
         <div
-            // className={`p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${cssStyle.fahad} ${subNav ? 'space-x-1' : ''} ${sub ? 'hidden hover:block' : ''}`}
             className={`p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 translate-x-1 flex items-center ${cssStyle.fahad} ${subNav ? 'space-x-1' : ''} ${cssStyle.show}`}
         >
             {children}
--- src/app/components/Navbar/navbar.tsx
+++ src/app/components/Navbar/navbar.tsx
@@ -1,51 +1,7 @@
-// "use client"
-// import { IoIosArrowUp } from 'react-icons/io'
-// import cssStyle from './button.module.css'
-
-// const CustomButton = ({ children, disabled, subNav, sub }: any) => {
-//     return (
-//         <div
-//             className={`relative p-2 text-yellow-50 ${!disabled ? 'hover:border-b-2 hover:border-t-2 hover:border-t-transparent hover:pt-[5px]' : ''} hover:cursor-pointer duration-300 flex items-center ${subNav ? 'group' : ''}`}
-//         >
-//             {children}
-//             {subNav && (
-//                 <span className={`${cssStyle.rotate} ml-2`}>
-//                     <IoIosArrowUp />
-//                 </span>
-//             )}
-//             {sub && (
-//                 <div className="absolute left-0 hidden group-hover:flex flex-col bg-gray-800 rounded shadow-lg mt-2 space-y-1 z-10">
-//                     {sub.map((v: any, index: number) => (
-//                         <a key={index} href={v.link} className="p-2 text-yellow-50 hover:bg-gray-700">
-//                             {v.name}
-//                         </a>
-//                     ))}
-//                 </div>
-//             )}
-//         </div>
-//     )
-// }
-
-// export default CustomButton
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React from 'react'
 import CustomButton from '../Button/button'
 import Image from 'next/image'
 import Logo from '../../assets/flogo.png'
-import cssStyle from '../Button/button.module.css'
 
 const Navbar = () => {
     return (
@@ -69,19 +25,8 @@ const Navbar = () => {
                 <CustomButton>
                     <a href="/contact">Contact</a>
                 </CustomButton>
-                <CustomButton subNav={true} sub={[{ name: 'Service0', link: '/services0' }, { name: 'Service1', link: '/services1' }, { name: 'Service2', link: '/services2' }, { name: 'Service0', link: '/services0' }]}
-                // className={cssStyle.show}
-                >
+                <CustomButton subNav={true} sub={[{ name: 'Service0', link: '/services0' }, { name: 'Service1', link: '/services1' }, { name: 'Service2', link: '/services2' }, { name: 'Service0', link: '/services0' }]}>
                     <a href="/services">Service</a>
-                    {/* <CustomButton subNav={true} sub={true}>
-                        <a href="/services">Service0</a>
-                    </CustomButton>
-                    <CustomButton subNav={true} sub={true}>
-                        <a href="/services">Service1</a>
-                    </CustomButton>
-                    <CustomButton subNav={true} sub={true}>
-                        <a href="/services">Service2</a>
-                    </CustomButton> */}
                 </CustomButton>
                 <CustomButton>
                     <a href="/contact">More</a>
@@ -92,7 +37,7 @@ const Navbar = () => {
                     <a href="/">Login</a>
                 </CustomButton>
             </span>
-        </div >
+        </div>
     )
 }
 
